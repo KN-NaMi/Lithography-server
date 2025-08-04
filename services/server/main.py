d17ff84b3ce3d9b1310997977c7015fc1fc941e1
Jakub Klimkowski <sadgi.enjoyer@protonmail.com>
2025-08-04 21:40:41 +0200

    fix: ensure proper indentation and formatting in main.py

--- services/server/main.py
+++ services/server/main.py
@@ -1,9 +1,13 @@
+import time
 from utils.nami_protocol import NaMiDevice
+
 
 def run():
     device = NaMiDevice()
     device.start()
     print("NaMiDevice started and listening for UDP discovery requests...")
-    
+    while True:
+        time.sleep(1)
+
 if __name__ == "__main__":
     run()
